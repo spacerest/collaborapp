6f985a4d37586f995c6fc9b588da5790e4ebb2ca
spacerest <sadiemparker@gmail.com>
2020-04-08 14:20:32 +0300

    remove signup/login from notes app

--- collaborapp/notes/views.py
+++ collaborapp/notes/views.py
@@ -6,46 +6,6 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from notes.models import Note
 
-
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'signup.html', {'form': form})
-    else:
-        form = SignupForm()
-        return render(request, 'signup.html', {'form': form})
-
-def login_user(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'login.html', {'form': form})
-    else:
-        form = AuthenticationForm()
-        return render(request, 'login.html', {'form': form})
-
-def logout_user(request):
-    logout(request)
-    return redirect('/login')
-
 @login_required(login_url='/login')
 def home(request):
     notes = Note.objects.filter(author=request.user).order_by('-created')
